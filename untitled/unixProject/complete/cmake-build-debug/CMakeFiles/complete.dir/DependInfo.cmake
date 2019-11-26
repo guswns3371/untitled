1065cea0eb3afef9dbd735225d68380a9484fcb4
hongseop <hongseopshin350@gmail.com>
2019-11-26 21:25:48 +0900

    dfd
    dfdf

---
+++ untitled/unixProject/complete/cmake-build-debug/CMakeFiles/complete.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/display.c" "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/cmake-build-debug/CMakeFiles/complete.dir/display.c.obj"
+  "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/function.c" "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/cmake-build-debug/CMakeFiles/complete.dir/function.c.obj"
+  "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/mcdonald.c" "C:/Users/hong/Downloads/untitled-master/untitled/unixProject/complete/cmake-build-debug/CMakeFiles/complete.dir/mcdonald.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ untitled/unixProject/complete/cmake-build-debug/CMakeFiles/complete.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/complete.dir/display.c.obj"
+  "CMakeFiles/complete.dir/function.c.obj"
+  "CMakeFiles/complete.dir/mcdonald.c.obj"
+  "complete.exe"
+  "complete.exe.manifest"
+  "complete.pdb"
+  "libcomplete.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/complete.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
